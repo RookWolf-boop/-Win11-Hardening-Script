6848dfe00141a12d2b662afd3e263b1d63c4b8dd
jesse <Jesse.rinehart@student.centraltech.edu>
2025-09-10 13:48:02 -0500

    aaaa

--- harden.ps1
+++ harden.ps1
@@ -9,3 +9,29 @@ try {
     Write-Host "Failed to create user '$NewUsername': $_" 
 }
 
+# Check for Administrator privileges
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Host "Restarting script as Administrator..."
+    $script = $MyInvocation.MyCommand.Definition
+    Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$script`"" -Verb RunAs
+    exit
+}
+# ...existing code...# Get all local user accounts
+$users = Get-LocalUser
+
+foreach ($user in $users) {
+    $username = $user.Name
+    # Prompt for authorization, default is Y
+    $response = Read-Host "Is '$username' an Authorized User? (Y/n) [Default: Y]"
+    if ($response -eq "") { $response = "Y" }
+    if ($response -eq "n" -or $response -eq "N") {
+        try {
+            Remove-LocalUser -Name $username
+            Write-Host "User '$username' has been deleted."
+        } catch {
+            Write-Host "Failed to delete user '$username': $_"
+        }
+    } else {
+        Write-Host "User '$username' is authorized."
+    }
+}
